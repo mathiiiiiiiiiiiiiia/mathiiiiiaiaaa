69046458a5fe044ca281dfd26436dba79cd5d560
mathiiiiiiiiiiiiiia <162451827+mathiiiiiiiiiiiiiia@users.noreply.github.com>
2024-03-27 10:42:00 -0300

    Add files via upload

---
+++ scrips.js
@@ -0,0 +1,36 @@
+//Declarando las variables string
+
+//strings
+let bienvenida = "wena cabros hoy andamo con pagina we, se vienen cositas"
+let presentacion = "soy el mathi de mi cerrú marrón y van a ver esta vaina sin sentido"
+
+
+let nombre ="mathias"
+let apellido = "Rosales"
+let usuario = "SpiderCR_123"
+
+//integer
+
+let edad = 16
+let altura = 1.65
+let peso = "63 kilos"
+
+//booleans
+
+let metalero = true
+let Estoy_muerto = false
+let peruano = true
+let ilegal = false
+
+//arreglos
+
+let Año_de_mi_mayor_desgracia = [cumple_el, 16,/11,/07]
+let Datos = [0,1,a,b,c]
+
+//objetos
+let Usuario = {Nombre: "Mathias", edad: 16 ,altura: 1.65 , peso: "63kilos"} 
+let objeto = {Circulo: "Esfera", triangulo: "piramide", cuadrado: "Cubo"}
+
+let byebye = "mewing"
+
+console.log("hola mundo")
